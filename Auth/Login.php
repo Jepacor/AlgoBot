45da7308d96f67a2586561c23bcf8c3acb0ab931
jepacor <Jepacor@gmail.com>
2022-05-10 16:52:01 +0200

    Création du système d'authentification

---
+++ Auth/Login.php
@@ -0,0 +1,27 @@
+<?php
+require_once ("../Testing/MyPDO.php");
+if(!isset($_POST['login']) || isset($_POST['password'])){
+    header('Location: LogForm.php');
+}
+$requete = "SELECT * FROM Utilisateurs WHERE Username = :username";
+$reponse = MyPDO::requeteStandard($requete, array(':username' => $_POST['login']));
+if($reponse == null){
+    //Utilisateur non trouvé
+    header('Location: LogForm.php');
+}
+else{
+    $dateInscription = $reponse[0]['date_inscription'];
+    $passwordTrue = $reponse[0]['password'];
+    $passwordUser = $dateInscription.$_POST['password'];
+    $passwordUser = hash('sha3-256', $passwordUser);
+    if($passwordUser == $passwordTrue){
+        //Utilisateur trouvé
+        session_start();
+        $_SESSION['username'] = $_POST['login'];
+        header('Location: ../Blockly/Blockly_Test.php');
+    }
+    else{
+        //Mauvais mot de passe
+        header('Location: LogForm.php');
+    }
+}
