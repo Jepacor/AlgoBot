02b5ff9ae7d14c4a36bf20cc314b806022ba7720
jepacor <Jepacor@gmail.com>
2022-05-18 14:08:58 +0200

    Déplacements du robot, première version

--- Robot/Robot.js
+++ Robot/Robot.js
@@ -1,10 +1,17 @@
 class Robot extends HTMLElement {
+
+
     constructor() {
         super();
+        this.x = 0;
+        this.y = 0;
     }
 
     connectedCallback() {
-        this.innerHTML = `<img src="Sprites/Robot_right.png" alt="loader" class="loader" style="position : absolute; left: 0px;top:0px">`;
+        this.style.position = "absolute";
+        this.style.left = "0px";
+        this.style.top = "0px";
+        this.innerHTML = `<img src="Sprites/Robot_right.png" alt="loader" class="loader">`;
     }
 
     static get observedAttributes() {
@@ -15,22 +22,33 @@ class Robot extends HTMLElement {
 
     moveRight() {
         this.firstChild.setAttribute('src', 'Sprites/Robot_right2.png');
-        this.firstChild.style.left = this.firstChild.style.left + 10 + 'px';
+        this.x += 10;
+        this.style.left = this.x + 'px';
     }
 
     moveLeft() {
         this.firstChild.setAttribute('src', 'Sprites/Robot_left.png');
-        this.firstChild.style.left = this.firstChild.style.left - 10 + 'px';
+        this.x -= 10;
+        this.style.left = this.x + 'px';
     }
 
     moveUp() {
         this.firstChild.setAttribute('src', 'Sprites/Robot_up.png');
-        this.firstChild.style.top = this.firstChild.style.top - 10 + 'px';
+        this.y -= 10;
+        this.style.top = this.y + 'px';
     }
 
     moveDown() {
-        this.firstChild.setAttribute('src', 'Sprites/Robot_up.png');
-        this.firstChild.style.top = this.firstChild.style.top + 10 + 'px';
+        this.firstChild.setAttribute('src', 'Sprites/Robot_down.png');
+        this.y += 10;
+        this.style.top = this.y + 'px';
+    }
+    resetRobot() {
+        this.firstChild.setAttribute('src', 'Sprites/Robot_right.png');
+        this.x = 0;
+        this.y = 0;
+        this.style.left = this.x + 'px';
+        this.style.top = this.y + 'px';
     }
 }
 customElements.define("sprite-robot", Robot);
